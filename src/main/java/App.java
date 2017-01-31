71aad323be215de6de5bc0b351bfc156919fbce1
Brad <bradleydegenaars@gmail.com>
2017-01-30 21:51:13 -0700

    Initial commit of M101J classwork

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Bradley on 1/30/2017.
+ * PACKAGE_NAME
+ */
+public class App {
+    public static void main ( String[] args ) {
+
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Bradley on 1/30/2017.
+ * PACKAGE_NAME
+ */
+public class AppTest {
+}
